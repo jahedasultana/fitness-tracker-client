3f5ba1a73fb63ea26dd5c89fb8d740bd45d284ae
jaheda8256 <jahedasultana47@gmail.com>
2024-09-15 23:17:22 +0600

    create new color

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,13 +1,10 @@
 const SectionTitle = ({ heading, subHeading }) => {
-    return (
-      <div className="mx-auto text-center md:w-4/12 my-8">
-        <p className="text-violet-500 font-semibold mb-2">_____
-          {subHeading}_____</p>
-          <p className="font-bold text-lg">------------------------------------------</p>
-        <h3 className="text-4xl font-bold uppercase py-4">{heading}</h3>
-          <p className="font-bold text-lg">------------------------------------------</p>
-      </div>
-    );
-  };
-  
-  export default SectionTitle;
+  return (
+    <div className="mx-auto text-center md:w-4/12 my-8">
+      <p className="text-yellow-600 mb-2">---{subHeading}---</p>
+      <h3 className="text-4xl uppercase border-y-4 py-4">{heading}</h3>
+    </div>
+  );
+};
+
+export default SectionTitle;
--- src/pages/Home/Home/OurTeam/OurTeam.jsx
+++ src/pages/Home/Home/OurTeam/OurTeam.jsx
@@ -9,12 +9,12 @@ const OurTeam = () => {
         heading="Our team"
       ></SectionTitle>
 
-<section className="py-6 bg-gray-800 dark:bg-gray-100 bg-gray-800 dark:bg-gray-100 text-gray-100 dark:text-gray-800 text-gray-100 dark:text-gray-800">
+<section className="py-6 dark:bg-gray-800 bg-gray-100  dark:text-gray-300 text-gray-100">
 	<div className="container flex flex-col items-center justify-center p-4 mx-auto space-y-8 sm:p-10">
 	
 		<div className="flex flex-row flex-wrap-reverse justify-center">
-			<div className="flex flex-col justify-center m-8 text-center bg-violet-300 p-8 rounded-2xl">
-				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full bg-gray-500 dark:bg-gray-500 bg-gray-500 dark:bg-gray-500" src="https://i.ibb.co/kMBt8t5/team-two.jpg" />
+			<div className="flex flex-col justify-center m-8 text-center bg-[#133c55] p-8 rounded-2xl">
+				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full bg-gray-500  dark:bg-gray-500" src="https://i.ibb.co/kMBt8t5/team-two.jpg" />
 				<p className="text-2xl font-semibold leading-tight">David Leea</p>
                 <p>------------------------------</p>
                 <p className="w-60 text-left"><span className="font-bold mr-2">Bio:</span>David is a professional trainer with a background in sports science. He specializes in strength training and weight loss programs</p>
@@ -22,8 +22,8 @@ const OurTeam = () => {
                 <p className="w-60 mt-2 text-left"><span className="font-bold mr-2">Expertise:</span> Yoga, Mindfulness, Holistic Wellness</p>
 				
 			</div>
-			<div className="flex flex-col justify-center m-8 text-center bg-violet-300 p-8 rounded-2xl">
-				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full bg-gray-500 dark:bg-gray-500 bg-gray-500 dark:bg-gray-500" src="https://i.ibb.co/kKnSMnD/team-three.jpg" />
+			<div className="flex flex-col justify-center m-8 text-center bg-[#133c55] p-8 rounded-2xl">
+				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full bg-gray-500 dark:bg-gray-500" src="https://i.ibb.co/kKnSMnD/team-three.jpg" />
 				<p className="text-2xl font-semibold leading-tight">Megan Brown</p>
                 <p>------------------------------</p>
                 <p className="w-60 text-left"><span className="font-bold mr-2">Bio:</span>Megan is a yoga instructor and wellness coach who has been helping clients achieve their fitness goals through holistic approaches.</p>
@@ -31,8 +31,8 @@ const OurTeam = () => {
                 <p className="w-60 mt-2  text-left"> <span className="font-bold mr-2">Expertise:</span>Performance Enhancement, Endurance Training, Sports Science</p>
 				
 			</div>
-			<div className="flex flex-col justify-center m-8 text-center bg-violet-300 p-8 rounded-2xl">
-				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full bg-gray-500 dark:bg-gray-500 bg-gray-500 dark:bg-gray-500" src="https://i.ibb.co/NVRwmKx/team-four.jpg" />
+			<div className="flex flex-col justify-center m-8 text-center bg-[#133c55] p-8 rounded-2xl">
+				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full bg-gray-500 dark:bg-gray-500" src="https://i.ibb.co/NVRwmKx/team-four.jpg" />
                
 				<p className="text-2xl font-semibold leading-tight">Alex Johnson</p>
                 <p>------------------------------</p>

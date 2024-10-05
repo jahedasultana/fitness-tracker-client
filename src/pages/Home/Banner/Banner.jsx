ee660cbc304f6f8c6cfa8ca89e64a6695a3a6c00
jaheda8256 <jahedasultana47@gmail.com>
2024-10-05 22:01:05 +0600

    added new change

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react";
 
 const images = [
   {
-    src: "https://img.freepik.com/premium-photo/modern-apartment-building-exterior-sunrise-perspective-view-modern-apartment-building-exterio_636537-279673.jpg?w=900",
-    text: "Welcome to Our Beautiful Apartment",
+    src: "https://i.postimg.cc/Gp3qKLHg/workout.jpg",
+   
   },
   {
-    src: "https://img.freepik.com/free-photo/analog-landscape-city-with-buildings_23-2149661456.jpg?t=st=1728036926~exp=1728040526~hmac=d04c41219b974fd0b6538c957d45c6ff7518028031ccd08146a9bdd6e3917d83&w=900",
-    text: "Explore the City Life",
+    src: "https://i.postimg.cc/MTpBgXSN/sportsm.jpg",
+   
   },
   {
-    src: "https://img.freepik.com/free-photo/3d-rendering-house-model_23-2150799725.jpg?t=st=1728041452~exp=1728045052~hmac=791f57ada1789729513b7b5fc362a9a111ab2d0d6f1df7a08d3880eb13d8e2e2&w=740",
-    text: "Discover Your Dream Home",
+    src: "https://i.postimg.cc/W3ydKnSq/men2.jpg",
+   
+  },
+  {
+    src: "https://i.postimg.cc/jjdQYM6M/woman.jpg",
+   
   },
 ];
 
@@ -59,9 +63,9 @@ const Banner = () => {
         {/* property details */}
         <div className="md:absolute -bottom-[60px] md:mt-0 mt-1 rounded-xl bg-white border border-gray-800/30 md:w-[70%] w-[90%] mx-auto md:p-6 p-4 shadow-md">
           <div className="flex justify-between md:flex-row flex-col md:space-y-0 space-y-3 md:relative">
-
-            <p className="md:absolute text-center -top-10 bg-[#1DB2FF] rounded-3xl py-1 px-5 text-sm font-medium text-white">Feature</p>
-
+          {/* <Link to='/allClasses'> */}
+            <button className="md:absolute text-center -top-10 bg-[#133c55] rounded-3xl py-2 px-5 text-sm font-medium text-white">Classes Page</button>
+            {/* </Link> */}
             <div className="md:space-y-5 space-y-2">
               <h5 className="md:text-2xl text-lg font-semibold text-black/80">
                 property name here || title

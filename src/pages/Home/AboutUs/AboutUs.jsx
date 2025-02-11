0570b74deaaea2666683623df8b1f2a28f8fe42f
jahedasultana <jahedasultana47@gmail.com>
2025-02-11 10:00:00 +0600

    AboutUs section re-design

--- src/pages/Home/AboutUs/AboutUs.jsx
+++ src/pages/Home/AboutUs/AboutUs.jsx
@@ -2,12 +2,12 @@ import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const AboutUs = () => {
   return (
-    <div className="my-20 bg-[#FFFFFF] dark:bg-[#1D232A]">
+    <div className="">
       <SectionTitle heading="About Us"></SectionTitle>
       <div>
-        <section className="bg-gray-100 dark:bg-[#133c55] dark:text-gray-700">
-          <div className="container flex flex-col-reverse mx-auto lg:flex-row rounded-2xl">
-            <div className="flex flex-col px-6 py-8 space-y-6 rounded-xl sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 bg-[#133c55] text-gray-300 ">
+        <section className="">
+          <div className="container flex flex-col-reverse mx-auto lg:flex-row rounded-2xl md:px-0 px-2">
+            <div className="flex flex-col px-2 md:px-6 md:py-8 py-2 space-y-6 rounded-xl sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 bg-[#133c55] text-gray-300">
               <div className="flex space-x-2 sm:space-x-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -24,10 +24,10 @@ const AboutUs = () => {
                   ></path>
                 </svg>
                 <div className="space-y-2">
-                  <p className="text-lg font-medium leading-snug">
+                  <p className="md:text-lg font-medium leading-snug">
                     State-of-the-Art Facilities
                   </p>
-                  <p className="leading-snug">
+                  <p className="leading-snug text-sm">
                     Our gym is equipped with the latest fitness equipment,
                     ensuring that you have everything you need for a
                     comprehensive workout.
@@ -50,10 +50,10 @@ const AboutUs = () => {
                   ></path>
                 </svg>
                 <div className="space-y-2">
-                  <p className="text-lg font-medium leading-snug">
+                  <p className="md:text-lg font-medium leading-snug">
                     Expert Trainers
                   </p>
-                  <p className="leading-snug">
+                  <p className="leading-snug text-sm">
                     Our team of certified trainers is passionate about fitness
                     and dedicated to helping you succeed. Whether you're looking
                     to build muscle, our trainers will create a personalized
@@ -77,10 +77,10 @@ const AboutUs = () => {
                   ></path>
                 </svg>
                 <div className="space-y-2">
-                  <p className="text-lg font-medium leading-snug">
+                  <p className="md:text-lg font-medium leading-snug">
                     Customized Training Plans
                   </p>
-                  <p className="leading-snug">
+                  <p className="leading-snug text-sm">
                     We understand that everyone’s fitness journey is unique. Our
                     trainers will work with you to develop a customized training
                     plan that fits your goals.
@@ -88,8 +88,8 @@ const AboutUs = () => {
                 </div>
               </div>
             </div>
-            <div className="lg:w-1/2 xl:w-3/5 bg-gray-200 dark:bg-[#133c55]">
-              <div className="flex items-center justify-center p-4 md:p-8 lg:p-12">
+            <div className="lg:w-1/2 xl:w-3/5 md:pb-0 pb-3">
+              <div className="flex items-center justify-center p-0 md:p-8 lg:p-12">
                 <img
                   src="https://i.ibb.co/VqGRpkx/3d-gym-equipment-23-2151114137.jpg"
                   alt=""
